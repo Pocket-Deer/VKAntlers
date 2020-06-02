e7bc9054bbcd857c1b70bb485944c9e203aa7a0c
Pocket-Deer <58855174+Pocket-Deer@users.noreply.github.com>
2020-06-02 22:11:03 +0700

    Update VKAntlers.user.js
    Добавлена менюшка через гит

--- VKAntlers.user.js
+++ VKAntlers.user.js
@@ -68,25 +68,19 @@ function DOM_ContentReady () {
 
         // Создание левых стилей и встраивание их на сайт
         $("body").append('<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">');
-        GM_addStyle ('.body_im {font-family: "Comfortaa", cursive !important; }');
-
+        //GM_addStyle ('.body_im {font-family: "Comfortaa", cursive !important; }');
 
         var mainMenu;
-        GM_xmlhttpRequest ( {
-            method: "GET",
-//            url: "https://raw.githubusercontent.com/Pocket-Deer/VKAntlers/master/mainmenu.html",
-            onload: function (response) {
-                console.log (response.responseText);
-                mainMenuFunction (response.responseText);
-            }
-        } );
-        // TODO: Скрипт выше выполняется ПОСЛЕ всего скрипта написанного тут, поэтому меню тупо не появляется, подумай над этим
-        function mainMenuFunction (somedata){
-            mainMenu = somedata;
-            alert(somedata);
-        }
-        console.log ("THAT WAS A MAINMENU OOOOOOOUT FUNCIOTN = " + mainMenu);
-   //     var mainMenu = '<div class="side"> <ul class="menu"> <li class="menu__list"> <a href="#">Пункт 1</a> <ul class="menu__drop"> <li> <a class="menu_toggle" id="1">Подпункт 1</a> </li> <li> <a class="menu_toggle" id="2">Подпункт 2</a> </li> </ul> </li> <li> <a href="#">Пункт 2</a> </li> <li class="menu__list"> <a href="#">Пункт 3</a> <ul class="menu__drop"> <li> <a class="menu_toggle" id="3">Подпункт 1</a> </li> <li> <a class="menu_toggle" id="4">Подпункт 2</a> </li> <li> <a class="menu_toggle" id="5">Подпункт 3</a> </li> <li> <a class="menu_toggle" id="6">Подпункт 4</a> </li> </ul> </li> <li> <a href="#">Пункт 4</a> </li> </ul> </div>';
+
+        $.ajax({ type: "GET",
+                url: "https://raw.githubusercontent.com/Pocket-Deer/VKAntlers/master/mainmenu.html",
+                async: false,
+                success : function(text)
+                {
+                    mainMenu = text;
+                }
+               });
+
         var mainMenu_css = '.side a { text-decoration: none; }  .side ul { margin: 0; padding: 0; list-style: none; } .side { z-index: 99999; display: none; text-decoration: none; width: 200px; margin: 10px; position: absolute; top: 47px; right: 10px; }  .menu > li > a { background-color: rgba(0, 0, 0, 0.5); color: #fff; padding: 10px; display: block; border-bottom: solid 1px #666; transition: 0.25s all; }  .menu_toggle { background-color: rgba(0, 0, 0, 0.5); color: #fff; padding: 10px; display: block; border-bottom: solid 1px #666; transition: 0.25s all; }  .menu a:hover { background-color: rgba(0, 0, 0, 0.35); }    /* Всплывающие пункты меню */    .menu__list, .menu__list_1 { position: relative; box-sizing: border-box; border-right: 4px solid rgb(0, 161, 255); }  .menu__drop, .menu__drop_1 { position: absolute; width: 100%; right: 100%; top: -99999em; transition: 0.25s opacity; opacity: 0; }  .menu__list:hover .menu__drop, .menu__list_1:hover .menu__drop_1 { opacity: 1; top: 0; }  /* Выбранный пункт меню */ .menu_toggle.menu_toggle_on { background-color: #768dff; }'
         var mainMenu_stylein = '<style type="text/css">';
         var mainMenu_styleout = '</style>';
