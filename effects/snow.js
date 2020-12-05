27867d7e8b9b21a153e21aee5e870add4131797c
Pocket-Deer <58855174+Pocket-Deer@users.noreply.github.com>
2020-12-05 19:18:40 +0700

    Update snow.js

--- effects/snow.js
+++ effects/snow.js
@@ -1,99 +1,64 @@
-/*!
-// Snow.js - v0.0.3
-// kurisubrooks.com
-*/
-
-// Amount of Snowflakes
-var snowMax = 35;
-
-// Snowflake Colours
-var snowColor = ["#DDD", "#EEE"];
-
-// Snow Entity
-var snowEntity = "&#x2022;";
-
-// Falling Velocity
-var snowSpeed = 0.75;
-
-// Minimum Flake Size
-var snowMinSize = 8;
-
-// Maximum Flake Size
-var snowMaxSize = 24;
-
-// Refresh Rate (in milliseconds)
-var snowRefresh = 50;
-
-// Additional Styles
-var snowStyles = "cursor: default; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; -o-user-select: none; user-select: none;";
-
-/*
-// End of Configuration
-// ----------------------------------------
-// Do not modify the code below this line
-*/
-
-var snow = [],
-	pos = [],
-	coords = [],
-	lefr = [],
-	marginBottom,
-	marginRight;
-
-function randomise(range) {
-	rand = Math.floor(range * Math.random());
-	return rand;
-}
-
-function initSnow() {
-	var snowSize = snowMaxSize - snowMinSize;
-	marginBottom = document.body.scrollHeight - 5;
-	marginRight = document.body.clientWidth - 15;
-
-	for (i = 0; i <= snowMax; i++) {
-		coords[i] = 0;
-		lefr[i] = Math.random() * 15;
-		pos[i] = 0.03 + Math.random() / 10;
-		snow[i] = document.getElementById("flake" + i);
-		//snow[i].style.fontFamily = "inherit";
-		snow[i].size = randomise(snowSize) + snowMinSize;
-		snow[i].style.fontSize = snow[i].size + "px";
-		snow[i].style.color = snowColor[randomise(snowColor.length)];
-		snow[i].style.zIndex = 1000;
-		snow[i].sink = snowSpeed * snow[i].size / 5;
-		snow[i].posX = randomise(marginRight - snow[i].size);
-		snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size);
-		snow[i].style.left = snow[i].posX + "px";
-		snow[i].style.top = snow[i].posY + "px";
-	}
-
-	moveSnow();
-}
-
-function resize() {
-	marginBottom = document.body.scrollHeight - 5;
-	marginRight = document.body.clientWidth - 15;
-}
-
-function moveSnow() {
-	for (i = 0; i <= snowMax; i++) {
-		coords[i] += pos[i];
-		snow[i].posY += snow[i].sink;
-		snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + "px";
-		snow[i].style.top = snow[i].posY + "px";
-
-		if (snow[i].posY >= marginBottom - 2 * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
-			snow[i].posX = randomise(marginRight - snow[i].size);
-			snow[i].posY = 0;
-		}
-	}
-
-	setTimeout("moveSnow()", snowRefresh);
-}
-
-for (i = 0; i <= snowMax; i++) {
-	document.body.innerHtml=("<span id='flake" + i + "' style='" + snowStyles + "position:absolute;top:-" + snowMaxSize + "'>" + snowEntity + "</span>");
-}
-
-window.addEventListener('resize', resize);
-window.addEventListener('load', initSnow);
+document.addEventListener('DOMContentLoaded', function(){
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+    script.onload = function(){
+        particlesJS("snow", {
+            "particles": {
+                "number": {
+                    "value": 200,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "opacity": {
+                    "value": 0.7,
+                    "random": false,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "size": {
+                    "value": 5,
+                    "random": true,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "line_linked": {
+                    "enable": false
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 5,
+                    "direction": "bottom",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": true,
+                        "rotateX": 300,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": false
+                    },
+                    "onclick": {
+                        "enable": false
+                    },
+                    "resize": false
+                }
+            },
+            "retina_detect": true
+        });
+    }
+    document.head.append(script);
+});
